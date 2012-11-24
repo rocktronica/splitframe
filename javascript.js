6715fd3acfc976a8fbc91fc2be431a07fed2f4a9
Tommy <tommy@iamnotagoodartist.com>
2012-11-24 15:04:00 -0800

    Init commit. Ready for testing!

---
+++ javascript.js
@@ -0,0 +1,55 @@
+(function() {
+
+	var bookmarklet = document.querySelector("[data-bookmarklet]");
+
+	if (window.top === window.self) {
+
+		// override prompts if run on this page
+		window.splitframe = {
+			needle: location.port ? "?local" : "splitframe/",
+			replacement: location.port ? "?live" : "splitframe/?live"
+		};
+
+		// remove any lingering params or hashes
+		var cleanUrl = window.location.href.replace(/(#.*|\?.*)/, "");
+		cleanUrl = cleanUrl.substr(0, cleanUrl.lastIndexOf("/"));
+		window.history.pushState(null, null, cleanUrl);
+
+		var path = location.origin + location.pathname;
+		path = path.replace(/\/*$/, "");
+
+		bookmarklet.href = 'javascript:(function(){var script = document.createElement("script");script.src =  "' + path + '/splitframe.js?" + (+new Date());document.body.appendChild(script);}());';
+
+		bookmarklet.addEventListener("click", function() {
+			// reset first frame's URL
+			var foundFrame = function() {
+				return document.querySelector("frame");
+			};
+			var waiting = setInterval(function() {
+				var frame = foundFrame();
+				if (frame) {
+					frame.src += "?local";
+					clearInterval(waiting);
+				}
+			}, 1);
+		});
+
+	} else {
+
+		bookmarklet.addEventListener("click", function(e) {
+			e.preventDefault();
+			console.warn("Wise guy.");
+		});
+
+		var match = location.search.match(/(local|live)/)
+		if (match && match.length) {
+			var param = match[1];
+			var a = document.querySelector("h1 a");
+			a.href += "?" + param;
+			a.innerHTML = a.innerHTML.replace(/split/i, "<span>" + param + "</span>");
+			document.body.classList.add(param);
+		}
+
+	}
+
+}());
---
+++ splitframe.js
@@ -0,0 +1,69 @@
+(function(win, doc, body, cache, undefined){
+
+    // get variables; first checking global then using prompt w/ cache
+    var app = "splitframe";
+    var needle = cache[app + "_needle"] = (win[app] && win[app].needle) ||
+        prompt("Development URL needle", cache[app + "_needle"] || "dev.");
+    var replacement = cache[app + "_replacement"] = (win[app] && win[app].replacement) ||
+        prompt("What replaces it on live site", cache[app + "_replacement"] || "www.");
+
+    // cancel if unused
+    if (!needle || !replacement) { return false; }
+
+    var url = location.href.replace(/#.*/, ""); // trim hash
+
+    // parent
+    var frameset = doc.createElement("frameset");
+    frameset.cols = "*,*";
+
+    // left frame
+    var localFrame = doc.createElement("frame");
+    frameset.appendChild(localFrame);
+    localFrame.src = url;
+
+    // right frame
+    var liveFrame = doc.createElement("frame");
+    frameset.appendChild(liveFrame);
+
+    doc.head.innerHTML = body.innerHTML = ""; // try to empty all content
+    body.setAttribute("style", "margin:0;padding:0;"); // reset css
+    body.appendChild(frameset);
+
+    // removes protocol, trailing slashes, and hash
+    function stripUrl(url) {
+        return url.replace(/(^.*\/{2}|\/*$)/g, "");
+    }
+
+    function fixLinks(context) {
+        var links = context.querySelectorAll("a"), count = links.length;
+        for (var i = 0; i < count; i++) {
+            var link = links[i];
+            if (link.host !== win.location.host) {
+                link.target = "_top";
+            }
+        }
+    }
+
+    // changing function, fired for both iframe loads and hash changes
+    var onChange = function() {
+        // since this may be either the iframe or window
+        var context = this.contentDocument || this.document;
+        var localUrl = context.location.href || context.location.href;
+
+        liveFrame.src = localUrl.replace(needle, replacement);
+
+        document.title = context.title;
+        history.pushState(null, null,
+            location.protocol + "//" + stripUrl(localUrl) + "/#!/" + stripUrl(liveFrame.src)
+        );
+
+        // make all external links kill frameset
+        fixLinks(context);
+
+        // re-bind hash change
+        localFrame.contentWindow.addEventListener("hashchange", onChange);
+    };
+
+    localFrame.addEventListener("load", onChange);
+
+}(window, document, document.body, localStorage));
